25e7a50acc4e93db9cd1e999914ec5a8b0f44098
kcastaneda1 <castanedaken89@gmail.com>
2026-05-04 15:03:40 -0700

    Delete connect_duck.py

--- connect_duck.py
+++
@@ -1,7 +0,0 @@
-import duckdb
-
-with duckdb.connect('health_care_fraud.duckdb') as con:
-    con.execute("""
-            CREATE TABLE healthcare_fraud AS 
-            SELECT * FROM read_csv_auto('data/healthcare_fraud_detection.csv')
-            """)
