6ccdd2d9185edfa124ae01238070b24cdd105a8e
kcastaneda1 <castanedaken89@gmail.com>
2026-05-04 10:28:18 -0700

    Update api_call_data.py

--- api_call_data.py
+++ api_call_data.py
@@ -8,8 +8,7 @@ DATA_PATH = 'data'
 
 if not os.path.exists(DATA_PATH):
     # download dataset only once
-    api.dataset_download_files(
-        "nudratabbas/healthcare-fraud-detection-dataset',
-        path=DATA_PATH,
-        unzip=True
+    api.dataset_download_files('nudratabbas/healthcare-fraud-detection-dataset'
+                               , path=DATA_PATH
+                               ,unzip=True
     )
