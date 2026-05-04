b7126db211301bbe1a93ac3cc3e155d58d033314
kcastaneda1 <castanedaken89@gmail.com>
2026-05-04 14:45:19 -0700

    Update connect_duck.py

--- connect_duck.py
+++ connect_duck.py
@@ -1,8 +1,7 @@
 import duckdb
 
-con = duckdb.connect('./data/health_care_fraud.duckdb')
-
-con.execute("""
+with duckdb.connect('health_care_fraud.duckdb') as con:
+    con.execute("""
             CREATE TABLE healthcare_fraud AS 
             SELECT * FROM read_csv_auto('data/healthcare_fraud_detection.csv')
             """)
