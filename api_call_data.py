e6089200e17ec625f1b7e6588bd2fab5e5ca5207
kcastaneda1 <castanedaken89@gmail.com>
2026-05-04 13:25:14 -0700

    Update api_call_data.py

--- api_call_data.py
+++ api_call_data.py
@@ -7,7 +7,7 @@ api= KaggleApi()
 api.authenticate()
 
 
-api.dataset_download_files("robiulhasanjisan/fraud-detection-healthcare", path="./data")
+api.dataset_download_files("nudratabbas/healthcare-fraud-detection-dataset" path="./data")
 
 import zipfile
 
