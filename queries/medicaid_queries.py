46a4dad8ee13767906fee17b87e65e0e4ea8239a
kcastaneda1 <castanedaken89@gmail.com>
2026-08-22 21:50:08 -0700

    Update medicaid_queries.py

--- queries/medicaid_queries.py
+++ queries/medicaid_queries.py
@@ -205,7 +205,7 @@ def get_call_center_trend(filters):
                 """                        
                 params.extend(states)
 
-        params += """
+        query += """
         AND reporting_date BETWEEN ? AND ?
         GROUP BY date_trunc('month', reporting_date), state_name 
         ORDER BY reporting_date, state_name
@@ -215,14 +215,6 @@ def get_call_center_trend(filters):
             filters['start_date'],
             filters['end_date']
         ])
-
-
-
-        print("\n--- CALL CENTER DEBUG ---")
-        print("Number of states:", len(states))
-        print("Number of ? placeholders:", query.count("?"))
-        print("Number of params:", len(params))
-        print("First 5 params:", params[:5])
 
         return con.execute(query,params).df()
     finally:
